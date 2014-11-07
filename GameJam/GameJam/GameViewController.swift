232f65584030ff53eeb09ee727c8670e37c82d57
remirobert <remi.robert@epitech.eu>
2014-11-07 22:08:31 +0100

    clean project

--- GameJam/GameJam/GameViewController.swift
+++ GameJam/GameJam/GameViewController.swift
@@ -9,41 +9,23 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : NSString) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+        let myScene = GameScene(size: self.view.frame.size)
             
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
+        // Configure the view.
+        let skView = self.view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
             
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
             
-            skView.presentScene(scene)
-        }
+        /* Set the scale mode to scale to fit the window */
+        myScene.scaleMode = .AspectFill
+        skView.presentScene(myScene)
     }
 
     override func shouldAutorotate() -> Bool {
